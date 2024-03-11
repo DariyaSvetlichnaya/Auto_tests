aa0e03dd7639dc15b470c5d425635792a03a0e2b
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-03-11 14:26:52 +0100

    hw

---
+++ hw_12/ccs_locators.py
@@ -0,0 +1,17 @@
+Header
+1. Header button: 'button.header__button.ng-tns-c1370900134-1'
+2. Catalog: 'button#fat-menu'
+3. Search field: 'input.search-form__input.ng-untouched.ng-pristine.ng-valid[placeholder='Я шукаю...']'
+4. Voice search: 'button.search-form__microphone.ng-star-inserted'
+5. Basket: 'rz-cart > button.header__button.ng-star-inserted'
+6. Home button: 'a.breadcrumbs__link[title='Інтернет-магазин Rozetka.ua - №1']'
+
+Products:
+7. Link to the Apple Iphone 15 Pro Max 256 MB in black color =
+'li.catalog-grid__cell rz-catalog-tile app-goods-tile-default a.product-link[href*='apple-iphone-15-pro-max-256gb-black-titanium']:nth-of-type(1)
+'
+8. Читати повністю кнопка = 'rz-read-all-btn button.toggle-btn-down.button--link'
+9. Next page button = 'a.button.button--gray.button--medium.pagination__direction.pagination__direction--forward.ng-star-inserted'
+
+Footer
+10. Download in AppStore button = 'li:nth-child(2) > a.footer-stores__button'
---
+++ hw_12/xpath_locators.py
@@ -0,0 +1,31 @@
+https://rozetka.com.ua/ua/mobile-phones/c80003/
+
+Header
+1. Header button = '//button[@class="header__button ng-tns-c1370900134-1"]'
+2. Catalog = '//button[@id="fat-menu"]'
+3. Search field = '//input[@class="search-form__input ng-untouched ng-pristine ng-valid" and @placeholder="Я шукаю..."]'
+4. Voice search = '//button[@class="search-form__microphone ng-star-inserted"]'
+5. Busket = '//rz-cart/button[@class="header__button ng-star-inserted"]'
+6. Home button = '//a[@class='breadcrumbs__link' and @title='Інтернет-магазин Rozetka.ua - №1']'
+
+Filters
+7. Бездротові технологіі = '(//button)[last()-26][@class="sidebar-block__toggle ng-star-inserted"]'
+8. Доставка > Готовий до відправлення (чекбокс) = '//a[@data-id="Готовий до відправлення"]'
+9. Продавець>Інші продавці = '(//li/a[@class="checkbox-filter__link"])[2]'
+10. Сортування (дропдаун) = '//select[@class="select-css ng-untouched ng-pristine ng-valid ng-star-inserted"]'
+11. Catalog view button = '//button[@class="catalog-view__button ng-star-inserted"]'
+
+Products:
+12. Link to the Apple Iphone 15 Pro Max 256 MB in black color =
+'(//li[contains(@class, 'catalog-grid__cell')]/rz-catalog-tile/app-goods-tile-default//a[contains(@class, 'product-link') and
+contains(@href, 'apple-iphone-15-pro-max-256gb-black-titanium')])[1]'
+13. Читати повністю кнопка = '//rz-read-all-btn/button[@class="toggle-btn-down button--link"]'
+14. Next page button = '//a[@class="button button--gray button--medium pagination__direction pagination__direction--forward ng-star-inserted"]'
+15. Move to the whishlist = '(//div[@class="wish-wrapper"]/button[@class="wish-button js-wish-button ng-star-inserted"])[2]'
+16. Compare button = '(//button[@class='compare-button ng-star-inserted'])[1]'
+
+Footer
+17. Download in AppStore button = '//li[2]/a[@class='footer-stores__button']'
+18. Графік роботи Call-центру = '//a[@class="button button--small button--with-icon button--link footer-top__schedule"]'
+19. Telegram link = '//footer[@class='footer']//a[@class='socials__link socials__link--telegram']'
+20. Оренда приміщень = '//a[contains(@class, 'ng-tns-c1400081795-0') and contains(@href, 'store_search') and text()=' Оренда приміщень ']'
