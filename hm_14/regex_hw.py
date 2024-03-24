7d442eeea56ec018b65ddbd1f103c3eeb948edfe
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-03-24 23:41:28 +0100

    regex fix

--- hm_14/regex_hw.py
+++ hm_14/regex_hw.py
@@ -6,7 +6,7 @@ my_string = "Place of delivery of goods: 82172, Ukraine, Lviv Region, Stryi, str
 if __name__ == '__main__':
     data = {
         'country': re.search(r'(?<=,\s)[A-Z][a-z]+', my_string).group(),
-        'region': re.search(r'[A-Z][a-z]+ Region', my_string).group(),
+        'region': re.search(r'L[a-z]+', my_string).group(),
         'city': re.search(r'S+[a-z]+', my_string).group(),
         'postal': re.search(r'[0-9]{5}', my_string).group(),
         'address': re.search(r'str\. [A-Z][a-z]+, [0-9]+', my_string).group(),
