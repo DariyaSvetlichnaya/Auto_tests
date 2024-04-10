59a006c2577f8a74875b2d3df0b8b850ac28407e
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-04-10 22:30:14 +0200

    docker

--- test/db_tests/test_get_products.py
+++ test/db_tests/test_get_products.py
@@ -1,6 +1,8 @@
 from assertpy import assert_that
+import pytest
 
 
+@pytest.mark.smoke
 def test_create_product(products_table):
     # Positive test: Ensure product is created successfully
     products_table.create_product()
@@ -9,6 +11,7 @@ def test_create_product(products_table):
     assert_that(len(products) > 0, "No products found in the database")
 
 
+@pytest.mark.smoke
 def test_get_product_by_name(products_table):
     # Positive test: Ensure product can be retrieved by name
     name = "Syrup"
@@ -18,6 +21,7 @@ def test_get_product_by_name(products_table):
     assert_that(product[0]).is_equal_to(name)
 
 
+@pytest.mark.smoke
 def test_create_product_with_empty_name(products_table):
     # Negative test: Attempt to create a product with an empty name
     initial_product_count = len(products_table.get_all_products())
@@ -33,6 +37,7 @@ def test_create_product_with_empty_name(products_table):
     assert initial_product_count == final_product_count
 
 
+@pytest.mark.smoke
 def test_create_product_with_invalid_name(products_table):
     # Negative test: Attempt to create a product with characters in the name
     initial_product_count = len(products_table.get_all_products())
