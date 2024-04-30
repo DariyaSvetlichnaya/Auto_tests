0e1c0f821b5213ae58ab00e483358862bef47a03
DariyaSvetlichnaya <d.a.svetlichnaya@gmail.com>
2024-04-30 23:40:17 +0200

    docker

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+
+
+if __name__ == '__main__':
+    if os.getenv('MARKS', None):
+        os.system(f"pytest test -m {os.getenv('MARKS')} -vv")
+    else:
+        os.system(f"pytest test -vv")
